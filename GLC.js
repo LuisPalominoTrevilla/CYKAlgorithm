6c15c99dd5c5363add9a9b5eab18bd03ebf82170
LuisPalominoTrevilla <luispalominot3@gmail.com>
2018-10-20 16:24:03 -0500

    Finished cyk algorithm

--- GLC.js
+++ GLC.js
@@ -34,8 +34,67 @@ class GLC{
         return P;
     }
 
+    // Receives a CYK computed table and returns its derivation from the left
+    getDerivation(dp, cols, word){
+        let result = 'S=>';
+        let terminals = '';
+        let counter = 0;
+        let i = 0;
+        let j = cols;
+        let letter = 'S';
+        let stack = [];
+        while (counter <= cols) {
+            let found = false;
+            if (i == j) {
+                terminals += word[counter++];
+                result += terminals;
+                for (var s = stack.length-1; s >= 0; s--) {
+                    result += stack[s][2];
+                }
+                if (stack.length > 0){
+                    result += "=>";
+                    const element = stack.pop();
+                    i = element[0];
+                    j = element[1];
+                    letter = element[2];
+                }
+            }
+            for (var k = i; k < j; k++) {
+                const setA = dp[i][k]
+                const setB = dp[k+1][j]
+                for (var valA of setA){
+                    for(var valB of setB){
+                        const producers = this.productions[valA+valB];
+                        if (producers){
+                            for(var elem of producers) {
+                                if (elem == letter) {
+                                    result += terminals;
+                                    result += valA+valB;
+                                    for (var s = stack.length-1; s >= 0; s--) {
+                                        result += stack[s][2];
+                                    }
+                                    result += "=>";
+                                    stack.push([k+1,j,valB]);
+                                    j = k;
+                                    letter = valA;
+                                    found = true;
+                                    break;
+                                }
+                            }
+                            if (found) break;
+                        }
+                    }
+                    if (found) break;
+                }
+                if (found) break;
+            }
+        }
+        return result;
+    }
+
     // Use CYK algorithm to check if w belongs to GLC
     accepts(w){
+        if (w.length == 0) return "La cadena "+w+" no es aceptada por la gramática G";
         let dp = Array(w.length).fill().map(()=>Array(w.length).fill());
         // Preparation
         for (var i = 0; i < w.length; i++){
@@ -64,12 +123,14 @@ class GLC{
         }
         for (var symbol of dp[0][w.length-1]) {
             if (symbol == "S"){
-                return true;
+                return this.getDerivation(dp, w.length-1, w);
             }
         }
-        return false;
+        return "La cadena "+w+" no es aceptada por la gramática G";
     }
 }
+let gramatic = process.argv[2];
+let word = process.argv[3];
 
-let a = new GLC("S->AB|SS|AC|BD|BA A->a B->b C->SB D->SA");
-console.log(a.accepts('aabaabbb'));
+let glc = new GLC(gramatic);
+console.log(glc.accepts(word));
